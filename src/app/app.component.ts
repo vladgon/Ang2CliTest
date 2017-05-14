a253b7bbba37504831ddae6e1610c6f544872679
vladgon <vladgon@gmail.com>
2017-05-13 23:02:33 -0400

    Test Stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works';
 }

a93e3e2f56daed7b811e82b67c65b18bcffd7cc3
vladgon <vladgon@gmail.com>
2017-09-11 22:23:07 -0400

    Angular 4.3, Navigation

--- src/app/routs.ts
+++ src/app/routs.ts
@@ -3,5 +3,6 @@ import {EmptyComponent} from './empty/empty.component';
 import {VladComponent} from './vlad.component';
 
 export const GLOBAL_ROUTS: Routes = [
+    {path: '', redirectTo: 'welcome', pathMatch: 'prefix'},
     {path: 'welcome', component: VladComponent},
     {path: 'empty', component: EmptyComponent}];
